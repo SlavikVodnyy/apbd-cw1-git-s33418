f79895935c51170c399284f4e59611caaea57b7e
Sviatoslav <s33418@pjwstk.edu.pl>
2026-03-10 16:55:29 +0100

    "2 commit"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            List<int> list = new() { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(list.Where(x => x % 2 == 0).Sum());
+        }
+
+        public static void Avg(List<int> list)
+        {
+            Console.WriteLine(list.Average());
         }
     }
 }
