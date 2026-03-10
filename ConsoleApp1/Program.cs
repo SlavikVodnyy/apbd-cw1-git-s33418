963ae454fc1afeb10f89054ca9d0dd941da92398
Sviatoslav <s33418@pjwstk.edu.pl>
2026-03-10 17:02:55 +0100

    3 commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,11 +7,18 @@
             Console.WriteLine("Hello, World!");
             List<int> list = new() { 1, 2, 3, 4, 5, 6 };
             Console.WriteLine(list.Where(x => x % 2 == 0).Sum());
+            Console.WriteLine("Enter 2 numbers: ");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(number1 * number2);
+            Avg(list);
+
         }
 
         public static void Avg(List<int> list)
         {
             Console.WriteLine(list.Average());
         }
+
     }
 }
